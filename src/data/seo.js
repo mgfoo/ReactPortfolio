fc09c2faa5d5fdaebfcf9d1ae7b1fb76f59d35e4
Michaela Foo <99845325+mgfoo@users.noreply.github.com>
2025-03-23 00:14:15 -0700

    Create pages + nav bar

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"EDIT HERE",
+		keywords: ["Michaela", "Michaela F", "Michaela Foo"],
+	},
+
+	{
+		page: "about",
+		description:
+			"EDIT HERE",
+		keywords: ["Michaela", "Michaela F", "Michaela Foo"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"EDIT HERE",
+		keywords: ["Michaela", "Michaela F", "Michaela Foo"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"EDIT HERE",
+		keywords: ["Michaela", "Michaela F", "Michaela Foo"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"EDIT HERE",
+		keywords: ["Michaela", "Michaela F", "Michaela Foo"],
+	},
+];
+
+export default SEO;
